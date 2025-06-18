de117b14ea10c602f3cfe896f5707086629956fd
Jakub Bednarek <kubabednarekk@gmail.com>
2025-06-18 08:05:50 +0200

    - Added favicon to the React frontend for better branding - Implemented automatic redirection from '/' to '/klienci' for improved UX

--- AuthenticationStudy/frontend/src/App.tsx
+++ AuthenticationStudy/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 import Clients from "./views/Clients";
 import Login from "./views/Login";
@@ -6,12 +6,12 @@ import Login from "./views/Login";
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/klienci" />} />
       <Route path="/klienci" element={<Clients />} />
       <Route path="/login" element={
-        <Login onLogin={(username, password) => {
-          console.log("Użytkownik:", username, "Hasło:", password);
-          // Tutaj możesz dodać żądanie do API
-        }} />
+      <Login onLogin={(username, password) => {
+        console.log("Użytkownik:", username, "Hasło:", password);
+      }} />
       } />
     </Routes>
   );
